ff687119b1f3187eda6662fb5e73f3c2ea8192ce
BanishedfromHalo23 <williamls1031@gmail.com>
2025-05-10 10:58:34 -0400

    task 0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,9 +5,14 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # Fix: decrement n to avoid infinite loop
     return result
 
 if __name__ == "__main__":
-    f = factorial(int(sys.argv[1]))
-    print(f)
+    try:
+        num = int(sys.argv[1])
+        if num < 0:
+            raise ValueError
+        print(factorial(num))
+    except (IndexError, ValueError):
+        sys.exit(1)
